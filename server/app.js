924bd964a79bab3cd6466f9d5265f2de69d28696
ciamac-da <ciamac.davoudi@gmail.com>
2021-02-23 09:10:41 +0100

    Verified by Marti4n v1.0.2

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
-const express = require('express')
+const express = require('express') //! To import express
+const graphqlHTTP = require('express-graphql') //! This package helps express to understand GraphQL
 
-const app = express();
+const app = express(); //! To store express package in a variable
 
 app.listen(4000,() => {
     console.log("Now listening for requests on port 4000")
