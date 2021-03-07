3d672da51056473349672e1d4cb0eeff7d00e9d8
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-07 11:55:57 +0100

    Verified by Marti4n v1.0.8 database and .env done!

--- server/app.js
+++ server/app.js
@@ -1,12 +1,20 @@
+if(process.env.NODE_ENV !== "production")
+require("dotenv").config()
 const express = require('express') //! To import express
 const schema = require('./schema/schema')
 const {graphqlHTTP} = require('express-graphql') //! This package helps express to understand GraphQL and provides a 
 //! simple way to create an express server . We use this as a middleware as a sigle routes
 
-
+const mongoose = require("mongoose")
 const app = express(); //! To store express package in a variable
 
-
+mongoose.connect( process.env.DB, 
+{ useNewUrlParser: true },
+{ useUnifiedTopology: true }
+)
+mongoose.connection.once('open',()=>{
+    console.log("Connected to the Database!")
+})
 //! Setting up a middleware!
 app.use('/graphql',graphqlHTTP({
     //! schema to represent how our graph looks
