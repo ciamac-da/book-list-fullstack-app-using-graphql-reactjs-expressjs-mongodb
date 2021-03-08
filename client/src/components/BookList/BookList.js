657846c15b3a597a7e0eff2ceba2d5c0fc6fa104
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-08 11:24:50 +0100

    Verified by Marti4n v1.2.2 Apollo Client added

---
+++ client/src/components/BookList/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    render() {
+        return ( 
+        <div>
+        <ul id="book-list">
+        <li>Book Name</li>
+        </ul> 
+        </div>
+        );
+    }
+}
+
+export default BookList;
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,13 +1,24 @@
-import React, {Component}  from 'react';
+import React, { Component } from 'react';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/client"
+import BookList from "../components/BookList/BookList"
+
+
+const client = new ApolloClient({
+    uri:'http://localhost:4000/grapgql'
+})
 
 class App extends Component {
-  render(){
-    return (
-      <div className="App">
-      <h1>Heyy</h1>
-       </div>
-   );
-  }
+    render() {
+        return ( 
+            <ApolloProvider client={client}>
+            <div className = "App" >
+            <h1> Heyy </h1> 
+            <BookList />
+            </div>
+            </ApolloProvider>
+        );
+    }
 }
 
 export default App;
