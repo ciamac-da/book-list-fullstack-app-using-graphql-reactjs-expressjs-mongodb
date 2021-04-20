7510cdb8674c629cc0e17c6b73f4d24ed3951f8a
ciamac-da <ciamac.davoudi@gmail.com>
2021-04-20 16:50:37 +0200

    Heroku???wtH

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+const client = new ApolloClient({
+    uri: "/graphql",
+    cache: new InMemoryCache(),
+});
+
+console.log(client);
+  
+ReactDOM.render(
+    <ApolloProvider client={client}>
+          <App/>
+    </ApolloProvider>
+    , document.getElementById('root')
+);
